4424a1bc1902c383a53905c1271c87cff63b1d76
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-08-13 16:53:03 -0300

    portal vagas

--- php_projects/PortalVagas/resources/views/usuarios/dashboard.blade.php
+++ php_projects/PortalVagas/resources/views/usuarios/dashboard.blade.php
@@ -1,3 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    
 <div class="container">
     <h3>Dashboard - Usuário</h3>
     <form action="{{route('usuarios.logout')}}" method="post">
@@ -9,3 +13,5 @@
         <span>voce esta logado</span>        
     @endif
 </div>
+
+@endsection
