8a01fae38b644bb20d4ca0c70a33f81ee4c43a6a
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2024-08-19 16:50:49 -0300

    escopo

--- php_projects/ConsultasMedicas/resources/views/parts/header.blade.php
+++ php_projects/ConsultasMedicas/resources/views/parts/header.blade.php
@@ -1,23 +1,32 @@
-@if (Auth::check())
-    <div>
-        <h3>Olá, {{Auth::user()->nome}}</h3>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+        <a class="navbar-brand" href="/">SeuLogo</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+            </ul>
+            <div class="d-flex">
+                @if (Auth::check())
+                    <div class="d-flex align-items-center">
+                        @if (Auth::user()->isMedico())
+                            <a href="/vagas" class="btn btn-primary me-2">Agendamento(médico)</a>
+                        @endif
+                        <span class="me-3">Bem-vindo, {{ Auth::user()->nome }}</span>
+                        <form action="/logout" method="post" class="d-inline">
+                            @csrf
+                            <button type="submit" class="btn btn-danger">Logout</button>
+                        </form>
+                    </div>
+                @else
+                    <a href="/login" class="btn btn-outline-primary me-2">Login</a>
+                    <a href="/registro" class="btn btn-warning">Sign-up</a>
+                @endif
+            </div>
+        </div>
     </div>
-    <div>
-        <form action="/logout" method="post">
-        @csrf
-        <input type='submit' value='Sair'>
-        </form>
-    </div>
-    <br>
-    <hr>
-    <br>
-@else
-    <div class="nav-bar">
-        <a href="/">Home</a>
-        <a href="/login">Login</a>
-        <a href="/registro">Registre-se</a>
-    </div>
-    <br>
-    <hr>
-    <br>
-@endif
+</nav>
